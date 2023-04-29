c17ac978497b4e5e675c2198eb7d2470391c3607
Ray Warner <raywarner@Rays-MBP.lan>
2023-04-29 15:29:59 -0400

    adding project files

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt>
+      			<div className="Tilt br2 shadow-2" style={{ height: '150px', width: '150px'}}>
+        			<div className="Tilt-inner pa3">
+        				<img style={{paddingTop: '10px'}} alt='logo' src={brain}/>
+        			</div>
+      			</div>
+    		</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
